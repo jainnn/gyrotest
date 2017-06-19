effb25d19742a6f19881fd76d048fc41dd847b34
jainnn <jainlee2016@gmail.com>
2017-06-20 05:40:36 +0900

    Update script.js

--- P5jsGyro/js/script.js
+++ P5jsGyro/js/script.js
@@ -2,34 +2,28 @@ var forceX = 0;
 var forceY = 0;
 var bulles = [];
 
-function windowResized() { // plein écran au resize aussi
+function windowResized() { 
 	resizeCanvas(windowWidth, windowHeight);
 }
 function setup(){
-	// une canvas plein écran
+
 	createCanvas(windowWidth, windowHeight);
 
-	// démarrer gyrojs, pour mettre à jour forceX et forceY
-	gyro.frequency = 10; // rafraichir 10 fois par secondes
+
+	gyro.frequency = 10; 
 	gyro.startTracking(function(o) {
-        // o.x, o.y, o.z for accelerometer
-        // o.alpha, o.beta, o.gamma for gyro
         forceX = o.gamma/50;
         forceY = o.beta/50;
     });
 
-    // création des bulles
-    for (var i = 0; i < 5; i++) {
+
+    for (var i = 0; i < 6; i++) {
     	bulles[i] = new Bulle();
     };
 }
 function draw(){
-	background('#FFFFFF'); // fond blanc
-
-	fill(0); // afficher les forces
-	text("forceX : "+forceX, 30, 30);
-	text("forceY : "+forceY, 30, 50);
-    fill('#ECECEC');
+	background(255); 
+        fill(30,50,220);
 
 	// update des bulles
 	for (var i = 0; i < bulles.length; i++) {
@@ -38,16 +32,18 @@ function draw(){
 }
 
 function Bulle(){
-	this.x = width/2; // démarre au centre
+	this.x = width/2; 
 	this.y = height/2;
 	this.vitx = random(2, 12);
 	this.vity = random(2, 12);
 	this.diam = random(20, 60);
 }
+
+
 Bulle.prototype = {
 	update: function(){
 
-		// la vitesse est proportionnée aux forces actuelles
+
 		this.x += this.vitx * forceX;
 		this.y += this.vity * forceY;
 
@@ -61,7 +57,7 @@ Bulle.prototype = {
 		} else if(this.y > height-this.diam/2){
 			this.y = height-this.diam/2;
 		}
-
-		ellipse(this.x, this.y, this.diam, this.diam);		
+                 rectMode(CENTER);
+		rect(this.x, this.y, this.diam, this.diam);		
 	}
 }
